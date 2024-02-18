8bc7e5e83f4b0e5621f40bec7db5e4edb89b8a56
Stefanyang222 <146639216+Stefanyang222@users.noreply.github.com>
2024-02-18 18:18:06 +1030

    Delete question13.sh

--- question13.sh
+++
@@ -1,7 +0,0 @@
-git checkout -b assignment-branch
-git rm *.sh
-git commit -m "Remove .sh files"
-echo "This is file13" > file13.txt
-git add file13.txt
-git commit -m "Add file13"
-git push origin assignment-branch
