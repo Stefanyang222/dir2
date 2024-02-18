211df2959ea892462c3b76dfc0a8fc1cf1147981
Stefanyang222 <146639216+Stefanyang222@users.noreply.github.com>
2024-02-18 18:20:49 +1030

    Update question09.sh

--- question09.sh
+++ question09.sh
@@ -1,6 +1,5 @@
 git branch branch1
 git checkout branch1
-touch file3.txt
-echo "This is file3 in branch1" > file3.txt
-git add file3.txt
+touch file3
+git add file3
 git commit -m "Add file3 to branch1"
