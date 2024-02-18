bb3960c225933ecfe689a0032322e52f0d0fc8e9
Stefanyang222 <146639216+Stefanyang222@users.noreply.github.com>
2024-02-18 18:07:29 +1030

    Update question13.sh

--- question13.sh
+++ question13.sh
@@ -1,7 +1,6 @@
-git checkout -b assignment-branch
+git checkout -b branchQ13
 rm *.sh
 touch file13.txt
-echo "This is file13" > file13.txt
 git add file13.txt
 git commit -m "Add file13"
-git push --set-upstream origin assignment-branch
+git push --set-upstream origin branchQ13
